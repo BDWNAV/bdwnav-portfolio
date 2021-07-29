87658526f7731e0587f0aeb67fc23d419c6ef62b
BDWNAV <rs736611@gmail.com>
2021-07-29 21:36:16 +1000

    Made some files also made a navbar

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import { Route, Switch } from 'react-router-dom';
+import Home from './pages/Home';
+
+class App extends React.Component {
+  render() {
+    return (
+      <main>
+        <Switch>
+          <Route path="/" component={Home}/>
+        </Switch>
+      </main>
+    )
+  }
+}
+
+export default App;
---
+++ src/components/Navbar.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+export default class Navbar extends React.Component {
+  render() {
+    return (
+      <div>
+        <ul className="navbar">
+          <li className="items"><a href="/">A</a></li>
+        </ul>
+      </div>
+    )
+  }
+}
