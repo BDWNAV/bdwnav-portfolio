ce1b731a3c8a9189bb16acd04da15fd1b4771ece
BDWNAV <rs736611@gmail.com>
2021-08-17 17:25:19 +1000

    Footer error
    Fixed the error where the footer was lagging.

--- src/pages/Projects.jsx
+++ src/pages/Projects.jsx
@@ -9,7 +9,7 @@ export default class Projects extends React.Component {
         <Navbar />
         <Cards image="https://cdn.discordapp.com/avatars/864059738789904384/44fe03349d6103b53b06bc967a1fab8b.webp?size=256" project="Jar-utilities" description="Jar-utilities is a discord bot for my up comming community server." projectUrl="https://github.com/BDWNAV/Jar-utilities" /> 
         <Cards image="https://cdn.discordapp.com/avatars/864059738789904384/44fe03349d6103b53b06bc967a1fab8b.webp?size=256" project="Jar hangout website" description="Jar hangout website is a website for my up comming community server." projectUrl="https://github.com/BDWNAV/jar-hangout-website" />
-        <footer className="copyright">© 2021 Bdwnav</footer>
+        <footer className="load copyright">© 2021 Bdwnav</footer>
       </div>
     )
   }
