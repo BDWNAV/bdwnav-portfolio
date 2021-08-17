bf361f90109609b5e0a8cbd216c35fdf43d85142
BDWNAV <rs736611@gmail.com>
2021-08-17 18:57:59 +1000

    Made a footer component.

---
+++ src/components/Footer.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+export default class Footer extends React.Component {
+  render() {
+    return (
+      <div className="load">
+        <div className="footer">
+          <p>© 2021 Bdwnav</p>
+        </div>
+      </div>
+    )
+  }
+}
