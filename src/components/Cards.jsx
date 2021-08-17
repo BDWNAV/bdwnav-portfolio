3c166d2625d7034d8d340c7ef3ffecddf53e38a4
BDWNAV <rs736611@gmail.com>
2021-08-17 14:36:37 +1000

    Added some things to the projects and page a fixed a error with the cards compoenent.

--- src/components/Cards.jsx
+++ src/components/Cards.jsx
@@ -4,10 +4,13 @@ export default class Cards extends React.Component {
   render() {
     return (
       <div className="load">
-        <div className="card">
-          <h1>{this.props.project}</h1>
-          <p>{this.props.description}</p>
-          <p><button className="card-button"><a href="https://github.com/BDWNAV/" style={{textDecoration: "none", color: "inherit"}}>Contact me</a></button></p>
+        <div className="card-container">
+          <div className="card">
+            <img src={this.props.image} alt="Github" style={{width: "100%"}} />
+            <h2>{this.props.project}</h2>
+            <p>{this.props.description}</p>
+            <p><button className="card-button"><a href={this.props.projectUrl} style={{textDecoration: "none", color: "inherit"}}>Repository</a></button></p>
+          </div>
         </div>
       </div>
     )
--- src/pages/Projects.jsx
+++ src/pages/Projects.jsx
@@ -7,7 +7,8 @@ export default class Projects extends React.Component {
     return (
       <div>
         <Navbar />
-        <Cards project="Test" description="Does this work"/>
+        <Cards image="https://cdn.discordapp.com/avatars/864059738789904384/44fe03349d6103b53b06bc967a1fab8b.webp?size=256" project="Jar-utilities" description="Jar-utilities is a discord bot for my up comming community server." projectUrl="https://github.com/BDWNAV/Jar-utilities" /> 
+        <Cards image="https://cdn.discordapp.com/avatars/864059738789904384/44fe03349d6103b53b06bc967a1fab8b.webp?size=256" project="Jar hangout website" description="Jar hangout website is a website for my up comming community server." projectUrl="https://github.com/BDWNAV/jar-hangout-website" />
       </div>
     )
   }
